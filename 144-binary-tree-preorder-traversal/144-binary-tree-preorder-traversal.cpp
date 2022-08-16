ac260fb8c005c1aa6ce0140cee6e813bfa28922c
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-08-16 23:31:25 +0530

    Time: 4 ms (41.74%), Space: 8.4 MB (76.83%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -12,20 +12,18 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>preorder;
-        if(root==NULL) return preorder;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            root=st.top();
-            st.pop();
-            preorder.push_back(root->val);
-            if(root->right !=NULL){
-                st.push(root->right);
-            }
-            if(root->left!=NULL)
-                st.push(root->left);
-        }
-        return preorder;
+        vector<int>ans;
+        if(!root)
+            return ans;
+        Treehelper(root,ans);
+        return ans;
+    }
+    void Treehelper(TreeNode *root,vector<int>&ans){
+        
+        if(!root)
+            return;
+        ans.push_back(root->val);
+        Treehelper(root->left,ans);
+        Treehelper(root->right,ans);
     }
 };
