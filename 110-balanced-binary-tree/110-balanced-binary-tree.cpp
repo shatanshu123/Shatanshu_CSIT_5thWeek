40542377ddba4f1ba75b6d80110674321b51118a
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-08-21 00:19:20 +0530

    Time: 16 ms (74.54%), Space: 20.9 MB (81.29%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -15,16 +15,15 @@ public:
         return dfsHeight(root)!=-1;
     }
     int dfsHeight(TreeNode *root){
-        if(root==NULL) return 0;
+        if(!root)return 0;
         int left=dfsHeight(root->left);
-        if(left==-1)
-            return -1;
+        if(left==-1) return -1;
         int right=dfsHeight(root->right);
-        if(right==-1)
-            return -1;
-        if(abs(left-right)>1)
-            return -1;
+        if(right==-1) return -1;
+        if(abs(left-right)>1) return -1;
         return max(left,right)+1;
     }
 };
+
+
 
