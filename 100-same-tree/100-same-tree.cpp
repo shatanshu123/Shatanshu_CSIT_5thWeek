f2303e841f6a7955aa21e2c6c1ce17c1c7e11ba8
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-08-21 19:47:02 +0530

    Time: 3 ms (67.54%), Space: 9.9 MB (78.00%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,16 +12,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q)
-            return 1;
-        if(!p || !q)
-            return 0;
-        if(p->val!=q->val)
-            return 0;
-        return isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
-        
+        if(!p && !q) return 1;
+        if(!p || !q) return 0;
+        if(p->val!=q->val) return 0;
+        return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
-
-
-
