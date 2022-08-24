dc2f69378e31a221a6bd3ec600d0a41b1061648b
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-08-24 23:50:52 +0530

    Time: 40 ms (34.75%), Space: 5.7 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return fmod((log10(n) / log10(3)), 1) == 0;
+       if(n==0) return false;
+        while(1){
+            if(n==1 || n==3) return true;
+            if(n%3!=0)return false;
+            n/=3;
+        }
+        return true;
     }
 };
+
