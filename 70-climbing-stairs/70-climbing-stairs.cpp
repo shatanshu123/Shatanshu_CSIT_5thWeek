69ddae6372d3f7757da4626ae6be785e89b70a6f
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-09-04 00:22:12 +0530

    Time: 3 ms (36.07%), Space: 6.2 MB (31.64%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> arr(n+1, 0);
+        arr[0] = arr[1] = 1;
+        for(int i=2; i<=n; ++i) arr[i] = arr[i-1]+arr[i-2];
+        return arr[n];
+    }
+};
