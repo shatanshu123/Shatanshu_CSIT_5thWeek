66be8fab5115b3730574663f55f3165f90036fc3
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-09-11 00:52:30 +0530

    Time: 13 ms (62.47%), Space: 17.3 MB (86.20%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,4 +1,3 @@
-//STRIVER_SDE_SHEET
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,29 +12,24 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        
-        if(!root) {
-            return 0;
-        }
-        int ans = 1;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-        while(!q.empty()) {
-            int n = q.size();
-            int start = q.front().second;
-            int end = q.back().second;
-            ans = max(ans, end-start+1);
-            for(int i=0; i<n; i++) {
-                auto p = q.front();
-                int idx = p.second-start;
+        if(!root)return 0;
+        int ans=0;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            int size=q.size();
+            int min=q.front().second;
+            int first,last;
+            for(int i=0;i<size;i++){
+                int cur_id=q.front().second-min;
+                TreeNode* node=q.front().first;
                 q.pop();
-                if(p.first->left) {
-                    q.push({p.first->left, 1ll*idx*2+1});
-                }
-                if(p.first->right) {
-                    q.push({p.first->right, 1ll*idx*2+2});
-                }
+                if(i==0)first=cur_id;
+                if(i==size-1)last=cur_id;
+                if(node->left)q.push({node->left,(long long)2*cur_id+1});
+                if(node->right)q.push({node->right,(long long)2*cur_id+2});
             }
+            ans=max(ans,last-first+1);
         }
         return ans;
     }
