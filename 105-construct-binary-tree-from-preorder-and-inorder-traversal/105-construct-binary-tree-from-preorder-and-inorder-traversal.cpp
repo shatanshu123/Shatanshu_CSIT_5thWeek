dde9e97eef98752a837df3aa7064380fe8b18bf9
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-09-13 00:29:46 +0530

    Time: 30 ms (65.90%), Space: 26.3 MB (43.43%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,32 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* solve(int start_pre, int start_in, int end_in, vector<int>& preorder, vector<int>& inorder) 
+    map<int,int> inmap;
+    int pre_ind=0;
+    TreeNode* rec(vector<int>& in,vector<int>& pre,int n,int in_st,int in_end)
     {
+        if(in_st>in_end)
+            return NULL;
+        int indx=inmap[pre[pre_ind]];
+        TreeNode* curr=new TreeNode(pre[pre_ind++]);
+        curr->left=rec(in,pre,n,in_st,indx-1);
+        curr->right=rec(in,pre,n,indx+1,in_end);
+        return curr;
         
-        if(start_in > end_in || start_pre > preorder.size()-1) return NULL;
-        
-        TreeNode* root = new TreeNode(preorder[start_pre]);
-        
-        int index = 0;
-        for(int i = start_in; i <= end_in; i++)
-            if(root->val == inorder[i]) index = i;
-            
-        root->left = solve(start_pre+1, start_in, index-1, preorder, inorder);
-        root->right = solve(start_pre+index-start_in+1, index+1, end_in, preorder, inorder);
-        return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-       return solve(0, 0, inorder.size()-1, preorder, inorder);
+        int n=inorder.size(); 
+        for(int i=0;i<n;i++)
+            inmap[inorder[i]]=i;
+         return rec(inorder,preorder,n,0,n-1);
     }
 };
