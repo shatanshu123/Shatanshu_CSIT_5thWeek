db5f892a8b59facf9703c0fc58bd5c60db0eb1aa
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-09-18 21:21:32 +0530

    Time: 17 ms (85.99%), Space: 20.6 MB (11.14%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,34 @@
+//STRIVER_SDE_SHEET
+
+class Solution {
+public:
+     int trap(vector<int>& height) {
+        int n=height.size();
+        
+        vector<int> left(n) , right(n);
+        left[0]=height[0];
+        right[n-1]=height[n-1];
+        
+        for(int i=1 ; i<n ;i++)
+        {
+            left[i] = max(left[i-1] , height[i]); // storing the maximum left wall for that ith 
+                                            // including itself
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=max(right[i+1] , height[i]);
+                          // storing the rightmost walls for ith index including itself 
+        }
+        
+        int ans =0;
+        
+        for(int i=0;i<n;i++)
+        {
+                              // take the minimum walls from rightmost & leftmost larger ones 
+            ans+=  min(right[i] , left[i]) - height[i];                // min(left[i] , right[i]) - height[i] 
+        }
+        
+        return ans;
+    }
+};
