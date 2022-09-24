ca756a3b0952ba2359d1fc96bb8191fbff6d4578
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-09-24 23:03:54 +0530

    Time: 74 ms (46.25%), Space: 36.8 MB (62.80%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,32 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class BSTIterator {
+    stack<TreeNode *> myStack;
+    bool reverse = true; 
+public:
+    BSTIterator(TreeNode *root, bool isReverse) {
+        reverse = isReverse; 
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for(;node != NULL; ) {
+             myStack.push(node);
+             if(reverse == true) {
+                 node = node->right; 
+             } else {
+                 node = node->left; 
+             }
+        }
+    }
+};
 class Solution {
 public:
-    void sol(TreeNode* root,vector<int>& v){
-        if(root==nullptr)return;
-        v.push_back(root->val);
-        if(root->left)sol(root->left,v);
-        if(root->right)sol(root->right,v);
-    }
-    
     bool findTarget(TreeNode* root, int k) {
-       unordered_map<int,int>m;
-        vector<int>v;
-        sol(root,v);
-        if(v.size()==1)return 0;
-         for(int i=0;i<v.size();i++)
-         { if(m.find(k-v[i])!=m.end())return true;
-         m[v[i]]++;
-         }
-return false;
+        if(!root) return false; 
+        BSTIterator l(root, false); 
+        BSTIterator r(root, true); 
+        
+        int i = l.next(); 
+        int j = r.next(); 
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
+        }
+        return false; 
     }
 };
